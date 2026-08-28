a31c936811daf056babf5ccf25018821ae77ebe6
giel-arch <hypergiel88@gmail.com>
2026-08-28 20:01:46 +0800

    Update device tree for TWRP 14.1 (omni -> twrp prefix)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_CPH2579.mk
+    $(LOCAL_DIR)/twrp_CPH2579.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_CPH2579-eng \
-    omni_CPH2579-userdebug
+    twrp_CPH2579-eng \
+    twrp_CPH2579-userdebug \
+    twrp_CPH2579-ap3a-eng \
+    twrp_CPH2579-ap3a-userdebug
