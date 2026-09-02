1805becd59c497ce1afaa17f5d01a3079eadae58
giel-arch <hypergiel88@gmail.com>
2026-09-02 15:44:01 +0800

    OrangeFox V7: Fix SDCard and OTG paths, simplify Data fstab, exclude default USB init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ OF_FL_PATH1 := /sys/class/flashlight_core/flashlight/flashlight_torch
 TW_BATTERY_SYSFS_PATH := /sys/class/power_supply/battery
 # TWRP Specific Flags
 RECOVERY_SDCARD_ON_DATA := true
-# TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 # TW_INCLUDE_CRYPTO := true
 # TW_INCLUDE_CRYPTO_FBE := true
